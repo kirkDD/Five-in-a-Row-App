f88bd47e395785ab5b16882bff7dd586d74ea55a
Leo Liao <leo211liao@gmail.com>
2020-06-18 20:01:16 -0700

    dwad

---
+++ FiveInaRow/app/src/main/java/com/example/fiveinarow/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.fiveinarow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.ValueAnimator;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    GameBoardView boardView;
+    FiveInARowGame game;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setUpBoardView();
+
+        // experiemnt
+        setUpStangeView();
+    }
+
+    void setUpBoardView() {
+        game = new FiveInARowGame(15);
+        boardView = new GameBoardView(this, game);
+        LinearLayout ll = findViewById(R.id.game_board_area);
+        ll.addView(boardView);
+    }
+
+    void setUpStangeView() {
+        LinearLayout ll = findViewById(R.id.strange_view);
+        ll.addView(new StrangeView(this));
+    }
+}
